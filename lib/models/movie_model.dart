751cebfac05ab58a18d4e10fb17d07bde9c8f02d
Jose Ortega <jose.ort98@gmail.com>
2023-03-14 10:46:10 -0500

    splash implementation, network checker, sharedPreferences

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,60 +1,59 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
 class Movie {
-    Movie({
-        required this.adult,
-        this.backdropPath,
-        required this.genreIds,
-        required this.id,
-        required this.originalLanguage,
-        required this.originalTitle,
-        required this.overview,
-        required this.popularity,
-        this.posterPath,
-        this.releaseDate,
-        required this.title,
-        required this.video,
-        required this.voteAverage,
-        required this.voteCount,
-    });
+  Movie({
+    required this.adult,
+    this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.originalLanguage,
+    required this.originalTitle,
+    required this.overview,
+    required this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    required this.title,
+    required this.video,
+    required this.voteAverage,
+    required this.voteCount,
+  });
 
-    bool adult;
-    String? backdropPath;
-    List<int> genreIds;
-    int id;
-    String originalLanguage;
-    String originalTitle;
-    String overview;
-    double popularity;
-    String? posterPath;
-    String? releaseDate;
-    String title;
-    bool video;
-    double voteAverage;
-    int voteCount;
+  bool adult;
+  String? backdropPath;
+  List<int> genreIds;
+  int id;
+  String originalLanguage;
+  String originalTitle;
+  String overview;
+  double popularity;
+  String? posterPath;
+  String? releaseDate;
+  String title;
+  bool video;
+  double voteAverage;
+  int voteCount;
 
-    String? heroId;
+  String? heroId;
 
-    get fullPosterImg {
-      if(posterPath != null) {
-        return 'https://image.tmdb.org/t/p/w500$posterPath';
-      }
-      return 'https://i.stack.imgur.com/GNhxO.png';
+  get fullPosterImg {
+    if (posterPath != null) {
+      return 'https://image.tmdb.org/t/p/w500$posterPath';
     }
+    return 'https://i.stack.imgur.com/GNhxO.png';
+  }
 
-    get fullBackdropPath {
-      if(posterPath != null) {
-        return 'https://image.tmdb.org/t/p/w500$backdropPath';
-      }
-      return 'https://i.stack.imgur.com/GNhxO.png';
+  get fullBackdropPath {
+    if (posterPath != null) {
+      return 'https://image.tmdb.org/t/p/w500$backdropPath';
     }
+    return 'https://i.stack.imgur.com/GNhxO.png';
+  }
 
-    factory Movie.fromJson(String str) => Movie.fromMap(json.decode(str));
+  factory Movie.fromJson(String str) => Movie.fromMap(json.decode(str));
 
-    factory Movie.fromMap(Map<String, dynamic> json) => Movie(
+  factory Movie.fromMap(Map<String, dynamic> json) => Movie(
         adult: json["adult"],
         backdropPath: json["backdrop_path"],
         genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
@@ -69,6 +68,22 @@ class Movie {
         video: json["video"],
         voteAverage: json["vote_average"].toDouble(),
         voteCount: json["vote_count"],
-    );
-}
+      );
 
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+        "id": id,
+        "original_language": originalLanguage,
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date": releaseDate,
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,33 +1,100 @@
+import 'dart:async';
+import 'dart:convert';
 
-
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:movies_app/models/movie_model.dart';
 import 'package:movies_app/providers/providers.dart';
 import 'package:movies_app/search/search_delegate.dart';
 import 'package:movies_app/widgets/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
-   
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  var _connectivityStatus = 'Unknown';
+  String movie = '';
+  late SharedPreferences preferences;
+  late Movie movies;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkConnectivity();
+    _connectivitySubscription =
+        Connectivity().onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        setState(() {
+          _connectivityStatus = 'Connected';
+        });
+      } else {
+        setState(() {
+          _connectivityStatus = 'Disconnected';
+        });
+      }
+    });
+  }
+
+  Future init() async{
+    preferences = await SharedPreferences.getInstance();
+
+    final movieJson = preferences.getString('movies');
+    if (movieJson == null)return;
+
+    final movies = Movie.fromJson(jsonDecode(movieJson));
+    setState(() {
+      this.movies = movies;
+    });
+  }
+
+  Future<void> _checkConnectivity() async {
+    final connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.mobile ||
+        connectivityResult == ConnectivityResult.wifi) {
+      setState(() {
+        _connectivityStatus = 'Connected';
+      });
+    } else {
+      setState(() {
+        _connectivityStatus = 'Disconnected';
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     final moviesProvider = Provider.of<MoviesProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Movies in cinema'),
-        actions: [ 
+        leading: Icon(_connectivityStatus == 'Connected'? Icons.network_wifi: Icons.perm_scan_wifi_outlined),
+        title: IconButton(
+          onPressed: () async {
+             final movieJson =jsonEncode(movies.toJson());
+             preferences.setString('movies', movieJson);
+          },
+          icon: const Icon(Icons.sync),
+        ),
+        actions: [
           IconButton(
-            onPressed: (){
-              showSearch(
-                context: context,
-                delegate: MovieSearchDelegate()
-              );
-            },
-            icon: const Icon (Icons.search_outlined)
-          ),
+              onPressed: () {
+                showSearch(context: context, delegate: MovieSearchDelegate());
+              },
+              icon: const Icon(Icons.search_outlined)),
         ],
       ),
       body: SingleChildScrollView(
@@ -35,17 +102,17 @@ class HomeScreen extends StatelessWidget {
           children: [
             //Main cards
             CardSwiperWidget(
-             movies:  moviesProvider.onDisplayMovies,
+              movies: moviesProvider.onDisplayMovies,
             ),
             // movies horizontal list
-             MovieSliderWidget(
-              movies: moviesProvider.popularMovies,
-              title: 'Popular Movies',
-              onNextPage: () => moviesProvider.getPopularMovies()
-            ),
+            MovieSliderWidget(
+                movies: moviesProvider.popularMovies,
+                title: 'Popular Movies',
+                onNextPage: () => moviesProvider.getPopularMovies()),
           ],
         ),
       ),
     );
   }
 }
+
